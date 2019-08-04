febfebb94ea78360a460cf739d004191025e66f6
Vopaaz <liyifan945@163.com>
2019-08-04 08:38:19 +0800

    Update tests for the renaming

--- tests/cached-reader-test.py
+++ tests/cached-reader-test.py
@@ -1,24 +1,24 @@
 import unittest
-from skutil.IO import CachedDataReader
+from skutil.IO import DataReader
 import logging
 import pandas as pd
 
 
-class CachedDataReaderTest(unittest.TestCase):
+class DataReaderTest(unittest.TestCase):
     def setUp(self):
         self.path1 = r"tests/assets/data1.csv"
         self.path2 = r"tests/assets/data2.csv"
         self.path3 = r"tests/assets/data3.csv"
 
     def test_singleton(self):
-        reader1 = CachedDataReader()
-        reader2 = CachedDataReader()
+        reader1 = DataReader()
+        reader2 = DataReader()
         self.assertTrue(reader1 is reader2)
 
-        reader3 = CachedDataReader(_id="test_singleton")
+        reader3 = DataReader(_id="test_singleton")
         self.assertTrue(reader1 is not reader3)
 
-        reader4 = CachedDataReader(_id="test_singleton")
+        reader4 = DataReader(_id="test_singleton")
         self.assertTrue(reader3 is reader4)
 
     def test_get_set_path_with_init(self):
@@ -26,7 +26,7 @@ class CachedDataReaderTest(unittest.TestCase):
         test_path = self.path2
         val_path = self.path3
 
-        reader = CachedDataReader(
+        reader = DataReader(
             train_path, test_path, val_path, _id="test_get_set_path_with_init")
 
         self.assertEqual(reader.train_path, train_path)
@@ -52,7 +52,7 @@ class CachedDataReaderTest(unittest.TestCase):
         test_path = self.path2
         val_path = self.path3
 
-        reader = CachedDataReader(_id="test_get_set_path_without_init")
+        reader = DataReader(_id="test_get_set_path_without_init")
 
         with self.assertRaises(AttributeError):
             _ = reader.train_path
@@ -81,52 +81,52 @@ class CachedDataReaderTest(unittest.TestCase):
     def test_initial_read_func_and_read_config(self):
         _id = "test_initial_read_func_and_read_config"
 
-        reader = CachedDataReader(
+        reader = DataReader(
             self.path1, _id=_id+"1", conf_a="a", conf_b="B")
-        self.assertTrue(reader._CachedDataReader__read_func is pd.read_csv)
-        self.assertDictEqual(reader._CachedDataReader__read_config, {
+        self.assertTrue(reader._DataReader__read_func is pd.read_csv)
+        self.assertDictEqual(reader._DataReader__read_config, {
                              "conf_a": "a", "conf_b": "B"})
 
-        reader = CachedDataReader(
+        reader = DataReader(
             self.path1, _id=_id+"2", read_func=pd.read_clipboard)
         self.assertTrue(
-            reader._CachedDataReader__read_func is pd.read_clipboard)
+            reader._DataReader__read_func is pd.read_clipboard)
 
     def test_duplicate_init(self):
         _id = "test_duplicate_init"
-        _ = CachedDataReader(self.path1, _id=_id, c_conf="some_conf")
+        _ = DataReader(self.path1, _id=_id, c_conf="some_conf")
 
         with self.assertRaises(ValueError):
-            _ = CachedDataReader(self.path2, _id=_id)
+            _ = DataReader(self.path2, _id=_id)
 
         with self.assertLogs(level=logging.WARNING):
-            _ = CachedDataReader(self.path1, self.path2, _id=_id)
+            _ = DataReader(self.path1, self.path2, _id=_id)
 
         with self.assertRaises(ValueError):
-            _ = CachedDataReader(self.path1, _id=_id, c_conf="some_else")
+            _ = DataReader(self.path1, _id=_id, c_conf="some_else")
 
         with self.assertRaises(ValueError):
-            _ = CachedDataReader(self.path1, _id=_id,
+            _ = DataReader(self.path1, _id=_id,
                                  c_conf="some_conf", c_conf_2="some_2")
 
         with self.assertLogs(level=logging.INFO):
-            _ = CachedDataReader(self.path1, _id=_id)
+            _ = DataReader(self.path1, _id=_id)
 
         with self.assertLogs(level=logging.INFO):
-            _ = CachedDataReader(self.path1, _id=_id, c_conf="some_conf")
+            _ = DataReader(self.path1, _id=_id, c_conf="some_conf")
 
         with self.assertLogs(level=logging.INFO):
-            _ = CachedDataReader(_id=_id, c_conf="some_conf")
+            _ = DataReader(_id=_id, c_conf="some_conf")
 
         with self.assertRaises(ValueError):
-            _ = CachedDataReader(read_func=pd.read_excel, _id=_id)
+            _ = DataReader(read_func=pd.read_excel, _id=_id)
 
         with self.assertLogs(level=logging.INFO):
-            _ = CachedDataReader(read_func=pd.read_csv, _id=_id)
+            _ = DataReader(read_func=pd.read_csv, _id=_id)
 
     def test_csv_reading(self):
         _id = "test_csv_reading"
-        reader1 = CachedDataReader(self.path1, _id=_id+"1")
+        reader1 = DataReader(self.path1, _id=_id+"1")
         self.assertTrue(
             (
                 reader1.train == pd.read_csv(self.path1)
@@ -145,7 +145,7 @@ class CachedDataReaderTest(unittest.TestCase):
             ).all()
         )
 
-        reader2 = CachedDataReader(
+        reader2 = DataReader(
             val_path=self.path2, _id=_id+"2", index_col="car")
         self.assertTrue(
             (
@@ -167,7 +167,7 @@ class CachedDataReaderTest(unittest.TestCase):
             ).all()
         )
 
-        reader_all = CachedDataReader(
+        reader_all = DataReader(
             self.path1, self.path2, self.path3, _id=_id+"all")
 
         self.assertTrue(
@@ -191,16 +191,16 @@ class CachedDataReaderTest(unittest.TestCase):
     def test_duplicate_csv_reading(self):
         _id = "test_duplicate_csv_reading"
 
-        reader1 = CachedDataReader(self.path1, _id=_id)
-        reader2 = CachedDataReader(self.path1, _id=_id)
-        reader3 = CachedDataReader(_id=_id)
+        reader1 = DataReader(self.path1, _id=_id)
+        reader2 = DataReader(self.path1, _id=_id)
+        reader3 = DataReader(_id=_id)
 
-        self.assertTrue(reader1.train is reader2.train)
-        self.assertTrue(reader2.train is reader3.train)
+        self.assertTrue(reader1.train is not reader2.train)
+        self.assertTrue(reader2.train is not reader3.train)
 
     def test_delay_set_path_and_read(self):
         _id="test_delay_set_path_and_read"
-        reader = CachedDataReader(_id=_id)
+        reader = DataReader(_id=_id)
 
         with self.assertRaises(AttributeError):
             _ = reader.train
