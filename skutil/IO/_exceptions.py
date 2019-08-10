aca2ddba20c9f09abc9d4daf6435b4ae2cefcad2
Vopaaz <liyifan945@163.com>
2019-08-10 19:58:29 +0800

    Feated: add check for unhandleable functions

--- skutil/IO/_exceptions.py
+++ skutil/IO/_exceptions.py
@@ -1,3 +1,7 @@
 class SpeculationFailedError(Exception):
     def __init__(self, message):
         super().__init__("Failed to save by speculation, "+message)
+
+class NotDecoratableError(Exception):
+    def __init__(self, obj):
+        super().__init__(f"{str(obj)} cannot be decorated by checkpoint, it may be a generator or a coroutine.")
