731ba902ad0c5264e4d13ba5d9c18328243a6bce
Vopaaz <liyifan945@163.com>
2019-08-11 23:22:22 +0800

    Fix: add f_globals to check the __file__, etc

--- tests/inline-checkpoint-test.py
+++ tests/inline-checkpoint-test.py
@@ -1,5 +1,25 @@
 from skutil.IO import InlineCheckpoint
 from checkpoint_test_base import R, CheckpointBaseTest
 
-def env():
-    pass
+
+def add_give_c(a, b):
+    c = "reset"
+    with InlineCheckpoint(watch=["a", "b"], produce=["c"], _id="add_give_c"):
+        R()
+        c = a + b
+    return c
+
+
+class InlineCheckpointTest(CheckpointBaseTest):
+    def test_add_give_c(self):
+        self.assertEqual(add_give_c(1,2),1+2)
+        self.runned()
+
+        self.assertEqual(add_give_c(1,2),1+2)
+        self.not_runned()
+
+        self.assertEqual(add_give_c(1,3),1+3)
+        self.runned()
+
+        self.assertEqual(add_give_c(1,3),1+3)
+        self.not_runned()
